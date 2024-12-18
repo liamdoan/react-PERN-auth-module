064a203a814fa9a0f7f2c29af011d944ebbe9f23
liamdoan <lamdoan9898@gmail.com>
2024-12-18 19:45:02 +0200

    use very email route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {signUp} = require("../controller/authControllerSignUp");
 const {logIn} = require("../controller/authControllerLogin");
 const {logOut} = require("../controller/authControllerLogout");
+const { verifyEmail } = require('../controller/authControllerVerifyEmail');
 
 router.post("/signup", signUp);
+router.post("/verify-email", verifyEmail);
 router.post("/login", logIn);
 router.post("/logout", logOut);
 
