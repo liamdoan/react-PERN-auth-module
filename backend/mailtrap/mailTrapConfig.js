208132707ae7def047784494a399809c325434b8
liamdoan <lamdoan9898@gmail.com>
2024-12-18 18:17:56 +0200

    set up config for mailtrap

--- backend/mailtrap/mailTrapConfig.js
+++ backend/mailtrap/mailTrapConfig.js
@@ -1,29 +1,14 @@
 const { MailtrapClient } = require("mailtrap");
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config()
 
 const TOKEN = process.env.MAILTRAP_TOKEN;
 
 module.exports.mailtrapClient = new MailtrapClient({
-    token: TOKEN,
+  token: TOKEN,
 });
 
 module.exports.sender = {
-    email: "hello@demomailtrap.com",
-    name: "Lam Doan",
+  email: "mailtrap@demomailtrap.com",
+  name: "Lam Doan",
 };
-// const recipients = [
-//     {
-//         email: "lamdoan9898@gmail.com",
-//     }
-// ];
-
-// client
-//     .send({
-//         from: sender,
-//         to: recipients,
-//         subject: "You are awesome!",
-//         text: "Congrats for sending test email with Mailtrap!",
-//         category: "Integration Test",
-//     })
-//     .then(console.log, console.error);
