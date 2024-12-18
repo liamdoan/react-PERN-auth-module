d222c0dfcd3eb9ddc66c4633d6a437957d2cf9a9
liamdoan <lamdoan9898@gmail.com>
2024-12-18 19:44:41 +0200

    add send welcome email function

--- backend/mailtrap/emailServices.js
+++ backend/mailtrap/emailServices.js
@@ -20,3 +20,23 @@ module.exports.sendVerificationEmail = async (email, verificationToken) => {
         console.error(error)
     }
 }
+
+module.exports.sendWelcomeEmail = async (email, userName) => {
+    const recipientEmail =[{email}];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipientEmail,
+            template_uuid: "e5acfa27-16e7-4c51-9154-ba7363eb62eb", // template created at MailTrap
+            template_variables: {
+                "company_info_name": "Lam Doan .Inc",
+                "name": userName
+            }
+        });
+
+        console.log("Welcome email sent!", response);
+    } catch (error) {
+        throw new Error("something wrong when sending welcome email", error);
+    }
+}
