714b6b1f56d0ee5b200bcf9a5605fde79ada4ca7
liamdoan <lamdoan9898@gmail.com>
2024-12-21 21:38:35 +0200

    put basic component to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
-import SignUp from './components/signUp'
-import Login from './components/login'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
 import Home from './components/Home'
 
 function App() {
