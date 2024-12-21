979700a74b91dc972bd632e19391471f759b1d62
liamdoan <lamdoan9898@gmail.com>
2024-12-21 19:46:01 +0200

    create and use basic pages in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import SignUp from './components/signUp'
+import Login from './components/login'
+import Home from './components/Home'
 
 function App() {
     return (
-        <>
-            hello
-        </>
+        <div className='main-wrapper'>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/sign-up" element={<SignUp />}/>
+                <Route path="/login" element={<Login />}/>
+            </Routes>
+        </div>
     )
 }
 
