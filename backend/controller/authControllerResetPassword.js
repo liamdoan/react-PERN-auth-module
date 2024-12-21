3862177b63e2fb9c773df6bc89178cd16370e420
liamdoan <lamdoan9898@gmail.com>
2024-12-21 17:50:57 +0200

    create controller for reset password

---
+++ backend/controller/authControllerResetPassword.js
@@ -0,0 +1,39 @@
+const { sendPasswordResetEmailSuccess } = require("../mailtrap/emailServices");
+const userModel = require("../models/userModel");
+const { generateHashPassword } = require("../utils/generateHashPassword");
+
+module.exports.resetPassword = async (req, res) => {
+    const {token} = req.params;
+    const {newPassword} = req.body;
+
+    try {
+        const user = await userModel.findOne({
+            resetPasswordToken: token,
+            resetPasswordExpiresAt: {$gt: Date.now()} //make sure token is not expired yet
+        });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Token invalid or expired!"
+            })
+        };
+
+        const newHashPassword = await generateHashPassword(newPassword);
+
+        user.password = newHashPassword;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpiresAt = undefined;
+
+        await user.save();
+
+        await sendPasswordResetEmailSuccess(user.email);
+
+        res.status(200).json({
+            message: "Password reset ok!"
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+}
