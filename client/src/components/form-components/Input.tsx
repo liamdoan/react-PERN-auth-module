dd220f69efe723a2363e2b9fef65b25177bf0812
liamdoan <lamdoan9898@gmail.com>
2024-12-21 21:17:57 +0200

    create input component

---
+++ client/src/components/form-components/Input.tsx
@@ -0,0 +1,23 @@
+import "./Input.css"
+
+interface InputProps{
+    type: string,
+    placeholder: string,
+    value: string,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const Input: React.FC<InputProps> = ({type, placeholder, value, onChange}) => {
+  return (
+    <>
+        <input
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+        />
+    </>
+  )
+}
+
+export default Input
