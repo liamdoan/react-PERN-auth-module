cf0f6b41d1a6f64112398a8bf35d7707e9d50e66
liamdoan <lamdoan9898@gmail.com>
2024-12-23 01:36:29 +0200

    move page components to page folder

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
+import Input from '../components/form-components/Input';
+import SubmitButton from '../components/form-components/SubmitButton';
+
+const Login = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleLogin = (e: any) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className="login-wrapper">
+            <div className='login-form'>
+                <h1>Log in</h1>
+                <form className='login-form-input' action="" onSubmit={handleLogin}>
+                    <Input
+                        type="text"
+                        placeholder='Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Input
+                        type="text"
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <SubmitButton title="Log In"/>
+                    <div className='notice-already-have-account'>
+                        <span className='notice-span'>
+                            Forgot your password?
+                            <span>&nbsp;</span>
+                        </span>
+                        <Link className='forgot-password-direct' to={"/forgot-password"}>Click here.</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ client/src/pages/SignUp.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './SignUp.css'
+import Input from '../components/form-components/Input';
+import SubmitButton from '../components/form-components/SubmitButton';
+import PasswordStrengthIndicator from '../components/form-components/PasswordStrengthIndicator';
+
+const SignUp = () => {
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleSignup = (e: any) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className="signup-wrapper">
+            <div className='signup-form'>
+                <h1>Create your account</h1>
+                <form className='signup-form-input' action="" onSubmit={handleSignup}>
+                    <Input
+                        type="text"
+                        placeholder='Full name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <Input
+                        type="text"
+                        placeholder='Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Input
+                        type="text"
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <PasswordStrengthIndicator password={password}/>
+                    <SubmitButton title="Sign Up"/>
+                    <div className='notice-already-have-account'>
+                        <span className='notice-span'>
+                            Already have an account?
+                            <span>&nbsp;</span>
+                        </span>
+                        <Link className='login-direct' to={"/login"}>Please login.</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp
