7b222c8691f83385e5b8ca1d474ec77ffaaca998
liamdoan <lamdoan9898@gmail.com>
2024-12-22 18:47:31 +0200

    use route for email verification code page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import SignUp from './components/SignUp'
 import Login from './components/Login'
 import Home from './components/Home'
+import EmailVerificationCode from './components/EmailVerificationCode'
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/sign-up" element={<SignUp />}/>
+                <Route path="/email-verification" element={<EmailVerificationCode />}/>
                 <Route path="/login" element={<Login />}/>
             </Routes>
         </div>
