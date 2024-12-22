e94b395e20a97b1af8df6fff8dfd84d2c9d96f73
liamdoan <lamdoan9898@gmail.com>
2024-12-22 18:01:14 +0200

    add disabled as props to button component

--- client/src/components/form-components/SubmitButton.tsx
+++ client/src/components/form-components/SubmitButton.tsx
@@ -2,12 +2,13 @@ import React from "react"
 import "./SubmitButton.css"
 
 interface SubmitButtonProps {
-    title: string
+    title: string,
+    disabled?: boolean
 }
 
-const SubmitButton: React.FC<SubmitButtonProps> = ({ title }) => {
+const SubmitButton: React.FC<SubmitButtonProps> = ({ title, disabled }) => {
   return (
-    <button type="submit">
+    <button type="submit" disabled={disabled}>
         {title}
     </button>
   )
