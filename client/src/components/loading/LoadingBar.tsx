7161594d24896f0f611df30918c457fa50dab93f
liamdoan <lamdoan9898@gmail.com>
2024-12-23 20:03:22 +0200

    add loading bar

---
+++ client/src/components/loading/LoadingBar.tsx
@@ -0,0 +1,11 @@
+import './LoadingBar.css';
+
+const LoadingBar = () => {
+    return (
+        <div className="loading-bar-container">
+            <div className="loading-bar"></div>
+        </div>
+    );
+};
+
+export default LoadingBar;
