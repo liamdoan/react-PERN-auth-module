ead5ef15b83a8b62874e6e33b9533e93389aece9
liamdoan <lamdoan9898@gmail.com>
2024-12-23 19:32:12 +0200

    use cors in backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connectMongo = require('./database/connectMongo.js');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
