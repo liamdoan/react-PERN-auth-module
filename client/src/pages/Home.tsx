3c27981fb90fc0d53a401095a02a57ee227a13be
liamdoan <lamdoan9898@gmail.com>
2024-12-24 18:01:41 +0200

    create homepage that user can only see after login, for now only as protected route, backend check is to be added.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,24 @@
+import { useSelector } from "react-redux"
+import { RootState } from "../redux/store"
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const user: any = useSelector((state: RootState) => state.user.user);
+    const isUserAuthenticated = useSelector((state: RootState) => state.user.isUserAuthenticated);
+    const navigate = useNavigate();
+
+    if (!user || !isUserAuthenticated) {
+        navigate('/login');
+        return;
+    }
+    
+    return (
+        <div>
+            <p>This is user homepage.</p>
+            <p>Hello: {user.name}</p>
+            <p>Your email is: {user.email}</p>
+        </div>
+    )
 }
 
 export default Home
