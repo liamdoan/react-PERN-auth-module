4519c90589e7158e6c77c1562353861d157a38cb
liamdoan <lamdoan9898@gmail.com>
2024-12-24 17:04:38 +0200

    set up cors to set cookie

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
+const cors = require('cors');
+
 const express = require('express');
 const connectMongo = require('./database/connectMongo.js');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
 
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
