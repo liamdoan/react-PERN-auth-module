a81eaab01ceeb947e7ee9029ff14ad84e4a55515
liamdoan <lamdoan9898@gmail.com>
2024-12-26 00:58:48 +0200

    create loading spinner

---
+++ client/src/components/loading/Spinner.tsx
@@ -0,0 +1,11 @@
+import './Spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="spinner">
+            <div className="loader"></div>
+        </div>
+    );
+};
+
+export default Spinner;
