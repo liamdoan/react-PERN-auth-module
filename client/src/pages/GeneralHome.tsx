2f7a5d6a6d1b6e282732b96823cb8e30c4ea73df
liamdoan <lamdoan9898@gmail.com>
2025-01-08 02:00:42 +0200

    create simple general homepage

--- client/src/pages/GeneralHome.tsx
+++ client/src/pages/GeneralHome.tsx
@@ -1,11 +1,26 @@
 import "./GeneralHome.css";
+import { Link } from 'react-router-dom';
 
 const GeneralHome = () => {
-  return (
-    <div>   
-        THIS IS THE GENERAL PAGE, EVERYBODY CAN SEE AKA GENERAL HOMEPAGE.
-    </div>
-  )
+    return (
+        <div>
+            <div className="navbar">
+                <h2>MERN AUTH</h2>
+                <div className="login-button">
+                    <Link className="navigate-link" to={"/login"}>Log in</Link>
+                </div>
+            </div>
+            <div className="main-section">
+                <h1>WELCOME TO MERN FULL AUTH MODEL</h1>
+                <div className="create-account-button">
+                    <Link className="navigate-link" to={"/sign-up"}>Create account</Link>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default GeneralHome
+
+// This GeneralHome page is to be replaced depending on each project
+// This is a simple home page to direct to Sign Up and Login page.
