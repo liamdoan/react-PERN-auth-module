4315cd3b00d5d2f33e68024190861c802a04a621
liamdoan <lamdoan9898@gmail.com>
2025-01-15 04:08:12 +0200

    add 4 calls, fetch all user data, update user roles, update user info, delete user

--- client/src/utils/authApiCalls.ts
+++ client/src/utils/authApiCalls.ts
@@ -83,3 +83,55 @@ export const resetPassword = async (token: string | undefined, newPassword: stri
         throw error;
     }
 };
+
+export const fetchAllUsersData = async () => {
+    try {
+        const response = await axios.get(`${BASE_URL}/get-all-users-data`, {
+            withCredentials: true
+        });
+
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const updateUserRoles = async (userId: string, updatedRoles: string[]) => {
+    try {
+        const response = await axios.patch(`${BASE_URL}/update-role/${userId}`, {
+            roles: updatedRoles
+        }, {
+            withCredentials: true
+        })
+
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const updateUserInfo = async (userId: string, name: string, email: string) => {
+    try {
+        const response = await axios.patch(`${BASE_URL}/modify-info/${userId}`, {
+            name, email
+        }, {
+            withCredentials: true
+        })
+
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const deleteUser = async (userId: string) => {
+    try {
+        const response = await axios.delete(`${BASE_URL}/delete-user/${userId}`, {
+            withCredentials: true
+        })
+
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
