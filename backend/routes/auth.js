ccac0c17f8c3603dd6cc0d6bd454f1e6fa4fbd4c
liamdoan <lamdoan9898@gmail.com>
2025-01-15 03:59:47 +0200

    create 4 endpoints to get all user data, assign roles to user, update user info, delete user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,8 +6,17 @@ const {logOut} = require("../controllers/auth-controllers/authControllerLogout")
 const { verifyEmail } = require('../controllers/auth-controllers/authControllerVerifyEmail');
 const { forgotPassword } = require('../controllers/auth-controllers/authControllerForgotPassword');
 const { resetPassword } = require('../controllers/auth-controllers/authControllerResetPassword');
+
 const { verifyToken } = require('../middleware/verifyToken');
+const { verifyAdminRole } = require('../middleware/verifyAdminRole');
+const { verifyAdminOrUserRole } = require('../middleware/verifyAdminOrUserRole');
+const { verifyAdminOrManagerRole } = require('../middleware/verifyAdminOrManagerRole');
+
 const { checkAuth } = require('../controllers/auth-controllers/authControllerCheckAuth');
+const { getAllUsersData } = require('../controllers/auth-controllers/role-specific-controllers/getAllUsersDataController');
+const { updateUserInfo } = require('../controllers/auth-controllers/role-specific-controllers/updateUserInfoController');
+const { updateUserRole } = require('../controllers/auth-controllers/role-specific-controllers/updateUserRoleController');
+const { deleteUser } = require('../controllers/auth-controllers/role-specific-controllers/deleteUserController');
 
 router.post("/signup", signUp);
 router.post("/verify-email", verifyEmail);
@@ -17,5 +26,10 @@ router.post("/login", logIn);
 router.post("/logout", logOut);
 
 router.get("/check-auth", verifyToken, checkAuth);
+
+router.get("/get-all-users-data", verifyToken, verifyAdminOrManagerRole, getAllUsersData);
+router.patch("/update-role/:userId", verifyToken, verifyAdminRole, updateUserRole);
+router.patch("/modify-info/:userId", verifyToken, verifyAdminOrUserRole, updateUserInfo);
+router.delete("/delete-user/:userId", verifyToken, verifyAdminOrUserRole, deleteUser);
 
 module.exports = router;
